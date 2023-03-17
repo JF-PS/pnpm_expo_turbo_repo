753596db1b408a71bbb1c5fc3077ef2795e758fb
Jeremy Trufier <jeremy@trufier.com>
2023-03-17 09:59:37 +0100

    fix: missing module

---
+++ apps/native/webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+const createExpoWebpackConfigAsync = require('@expo/webpack-config');
+
+function getWorkspaceModules() {
+  const { dependencies } = require('./package.json');
+  return Object
+    .entries(dependencies)
+    .filter(([,v]) => v.startsWith('workspace:'))
+    .map(([k]) => k)
+}
+
+module.exports = async function (env, argv) {
+  const config = await createExpoWebpackConfigAsync({
+    ...env,
+    babel: {
+      ...env.babel,
+      dangerouslyAddModulePathsToTranspile: getWorkspaceModules(),
+    }
+  }, argv);
+
+  return config;
+};
